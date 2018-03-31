24cd15bfa9e2dac674bf07c257bed5c0a9adc76c
Kelly Lampotang <klampotang@gmail.com>
2018-03-31 14:52:41 -0700

    breathe

--- Nope-ioids/Nope-ioids/ViewControllers/BreatheViewController.swift
+++ Nope-ioids/Nope-ioids/ViewControllers/BreatheViewController.swift
@@ -2,27 +2,18 @@ import UIKit
 
 class BreatheViewController: UIViewController {
     
+    @IBOutlet weak var BreatheImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let breatheGif = UIImage.gifImageWithName("breathe")
+        BreatheImageView.image = breatheGif
         
-        // Do any additional setup after loading the view.
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
 
--- Nope-ioids/Nope-ioids/ViewControllers/HomeScreenViewController.swift
+++ Nope-ioids/Nope-ioids/ViewControllers/HomeScreenViewController.swift
@@ -2,10 +2,14 @@ import UIKit
 
 class HomeScreenViewController: UIViewController {
     
+    @IBOutlet weak var DateLabel: UILabel!
+    @IBOutlet weak var DayLabel: UILabel!
+    @IBOutlet weak var WeekdayLabel: UILabel!
+    @IBOutlet weak var HelpStayStrongButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
+        HelpStayStrongButton.layer.cornerRadius = 0.5 * HelpStayStrongButton.bounds.size.width
+        HelpStayStrongButton.clipsToBounds = true
     }
     
     override func didReceiveMemoryWarning() {
@@ -13,15 +17,10 @@ class HomeScreenViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func HelpButtonPressed(_ sender: Any) {
+    }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+    @IBAction func SeeProgressButtonPressed(_ sender: Any) {
+        
+    }
 }
