8e75891f7b2bfe593f8780bd72aea03aa1b02bbc
Andrew Jiang <andrewji@usc.edu>
2018-03-31 23:56:50 -0700

    breathe in breathe out animations

--- Nope-ioids/Nope-ioids/ViewControllers/BreatheViewController.swift
+++ Nope-ioids/Nope-ioids/ViewControllers/BreatheViewController.swift
@@ -4,15 +4,21 @@ class BreatheViewController: UIViewController {
     
     @IBOutlet weak var NextButton: UIButton!
     @IBOutlet weak var BreatheImageView: UIImageView!
+    @IBOutlet weak var BreatheLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         let breatheGif = UIImage.gifImageWithName("breathe")
         BreatheImageView.image = breatheGif
         
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.5){
+            self.BreatheLabel.text = "Breathe out..."
+        }
+        
         NextButton.isHidden = true
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 6) { // change 2 to desired number of seconds
             // Your code with delay
+            self.BreatheLabel.isHidden = true
             self.NextButton.isHidden = false
         }
     }
