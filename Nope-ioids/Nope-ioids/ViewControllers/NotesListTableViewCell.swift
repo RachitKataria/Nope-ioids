f78772c8b2dcb0790dabe43277c474ffbb11ca1f
Kelly Lampotang <klampotang@gmail.com>
2018-03-31 17:25:21 -0700

    notes to self

---
+++ Nope-ioids/Nope-ioids/ViewControllers/NotesListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotesListTableViewCell.swift
+//  Nope-ioids
+//
+//  Created by Kelly Lampotang on 3/31/18.
+//  Copyright © 2018 Rachit Kataria. All rights reserved.
+//
+
+import UIKit
+
+class NotesListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Nope-ioids/Nope-ioids/ViewControllers/NotesSelfViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NotesSelfViewController.swift
+//  Nope-ioids
+//
+//  Created by Kelly Lampotang on 3/31/18.
+//  Copyright © 2018 Rachit Kataria. All rights reserved.
+//
+
+import UIKit
+
+class NotesSelfViewController: UIViewController {
+
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var NotesList: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func recordButtonPressed(_ sender: Any) {
+        
+    }
+    
+}
