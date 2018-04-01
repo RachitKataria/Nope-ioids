3aa1ab317cf20a02d1a8d45d82bacb9e68caf2e1
Kelly Lampotang <klampotang@gmail.com>
2018-03-31 20:07:21 -0700

    alert view

--- Nope-ioids/Nope-ioids/ViewControllers/HelpViewController.swift
+++ Nope-ioids/Nope-ioids/ViewControllers/HelpViewController.swift
@@ -14,7 +14,30 @@ class HelpViewController: UIViewController {
     }
     
     @IBAction func callFriendPressed(_ sender: Any) {
+        let alertController = UIAlertController(title: nil, message: "Choose a friend to call", preferredStyle: .actionSheet)
         
+        let friend1Action = UIAlertAction(title: "Call Mom", style: .default) { action in
+            // ...
+        }
+        alertController.addAction(friend1Action)
+        
+        let friend2Action = UIAlertAction(title: "Call Stacey", style: .default) { action in
+            // ...
+        }
+        alertController.addAction(friend2Action)
+        
+        let friend3Action = UIAlertAction(title: "Call Mentor John", style: .default) { action in
+            // ...
+        }
+        alertController.addAction(friend3Action)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { action in
+            // ...
+        }
+        alertController.addAction(cancelAction)
+        
+        self.present(alertController, animated: true){
+        }
     }
     
     @IBAction func communityPressed(_ sender: Any) {
--- Nope-ioids/Nope-ioids/ViewControllers/HomeScreenViewController.swift
+++ Nope-ioids/Nope-ioids/ViewControllers/HomeScreenViewController.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class HomeScreenViewController: UIViewController {
     
+    @IBOutlet weak var trackProgressButton: UIButton!
     @IBOutlet weak var StrongerCircle: UIImageView!
     @IBOutlet weak var DateLabel: UILabel!
     @IBOutlet weak var DayLabel: UILabel!
@@ -9,8 +10,14 @@ class HomeScreenViewController: UIViewController {
     @IBOutlet weak var HelpStayStrongButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        DateLabel.font = UIFont(name: ".SFUIText-Medium", size: 12)!
+        DayLabel.font = UIFont(name: ".SFUIText-Medium", size: 12)!
+        WeekdayLabel.font = UIFont(name: ".SFUIText-Medium", size: 12)!
         HelpStayStrongButton.layer.cornerRadius = 0.5 * HelpStayStrongButton.bounds.size.width
         HelpStayStrongButton.clipsToBounds = true
+        HelpStayStrongButton.titleLabel?.font = UIFont(name: ".SFUIText-Medium", size: 18)!
+        trackProgressButton.titleLabel?.font = UIFont(name: ".SFUIText-Medium", size: 18)!
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -22,10 +29,12 @@ class HomeScreenViewController: UIViewController {
         UIView.animate(withDuration: 1.5, animations: {
             self.StrongerCircle.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
         })
-/*
-        let storyboard: UIStoryboard = UIStoryboard(name: "BreatheStoryboard", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "BreatheViewController") as! BreatheViewController
-        self.show(vc, sender: self)*/
+        { (finished) in
+            let storyboard: UIStoryboard = UIStoryboard(name: "BreatheStoryboard", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "BreatheViewController") as! BreatheViewController
+            self.present(vc, animated: false, completion: nil)
+        }
+        
     }
     
     @IBAction func SeeProgressButtonPressed(_ sender: Any) {
@@ -34,3 +43,30 @@ class HomeScreenViewController: UIViewController {
         self.show(vc, sender: self)
     }
 }
+class FadeSegue: UIStoryboardSegue {
+    override func perform() {
+        // Get the view of the source
+        let sourceViewControllerView = self.source.view
+        // Get the view of the destination
+        let destinationViewControllerView = self.destination.view
+        
+        let screenWidth = UIScreen.main.bounds.size.width
+        let screenHeight = UIScreen.main.bounds.size.height
+        
+        // Make the destination view the size of the screen
+        destinationViewControllerView?.frame = CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight)
+        
+        // Insert destination below the source
+        // Without this line the animation works but the transition is not smooth as it jumps from white to the new view controller
+        destinationViewControllerView?.alpha = 0;
+        sourceViewControllerView?.addSubview(destinationViewControllerView!);
+        // Animate the fade, remove the destination view on completion and present the full view controller
+        UIView.animate(withDuration: 10, animations: {
+            destinationViewControllerView?.alpha = 1;
+        }, completion: { (finished) in
+            destinationViewControllerView?.removeFromSuperview()
+            self.source.present(self.destination, animated: false, completion: nil)
+        })
+    }
+}
+
